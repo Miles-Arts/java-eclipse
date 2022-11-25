2224bc98596166c4e046bcaf30931053a63b9440
Milton Figueredo Miles <gratno@gmail.com>
2022-11-25 18:50:57 -0500

    Float double

---
+++ java-primer-paso/src/TipoEntero.java
@@ -0,0 +1,16 @@
+
+public class TipoEntero {
+	
+	public static void main(String[] args) {
+		
+		int edadPersona = 28;
+		System.out.println(edadPersona);
+		
+		
+		edadPersona = 28 + 5; 
+		System.out.println(edadPersona);
+		
+		double salario = 1250.50;
+		System.out.println(salario);
+	}
+}
