140afd919272ce8ed72f6351f5ddc2b1108d6554
Milton Figueredo Miles <gratno@gmail.com>
2022-11-25 15:30:10 -0500

    Enteros en Java

---
+++ java-primer-paso/src/TipoVariable.java
@@ -0,0 +1,16 @@
+
+public class TipoVariable {
+	
+	public static void main(String[] args) {
+		System.out.println("Hola mundo!!!");
+	
+		int	edad = 38;
+		System.out.println(edad);
+		
+		edad = 25;
+		System.out.println(edad);
+		
+		edad = 25 + 5;
+		System.out.println(edad);
+	}
+}
