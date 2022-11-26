a26fd1652046d6a02d014119facdf4fd60b0c166
Milton Figueredo Miles <gratno@gmail.com>
2022-11-25 19:04:15 -0500

    Float- double -  y División

--- java-primer-paso/src/TipoEntero.java
+++ java-primer-paso/src/TipoEntero.java
@@ -6,11 +6,24 @@ public class TipoEntero {
 		int edadPersona = 28;
 		System.out.println(edadPersona);
 		
-		
 		edadPersona = 28 + 5; 
 		System.out.println(edadPersona);
 		
 		double salario = 1250.50;
 		System.out.println(salario);
+		
+		double edad = 28;
+		System.out.println(edad);
+		
+		int salario1 = 1259;
+		salario1 = 1250;
+		System.out.println(salario1);
+		
+		double salarioMitad = salario / 2;
+		System.out.println(salarioMitad);
+		
+		int salario2 = 1250;
+		int salarioMitad1 = salario2 / 3;
+		System.out.println(salarioMitad1);
 	}
 }
