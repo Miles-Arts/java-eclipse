a4703b86e95fc7cc1062f7f2576ba8715bdc81ec
Milton Figueredo Miles <gratno@gmail.com>
2022-11-26 18:36:21 -0500

    función char

---
+++ java-primer-paso/src/EjemploCaracteres.java
@@ -0,0 +1,21 @@
+
+public class EjemploCaracteres {
+	public static void main(String[] args) {
+		
+		char caracter = 'a';
+		System.out.println(caracter);
+		
+		caracter = 65;
+		System.out.println(caracter);
+		
+		caracter = 65 + 1;
+		System.out.println(caracter);
+		
+		char segundoCaracter = (char)(caracter + 1);
+		
+		System.out.println(segundoCaracter);
+		
+		
+		
+	}
+}
