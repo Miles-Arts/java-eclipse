483ca841b2068a5cdd598a90d3391018325e3454
Milton Figueredo Miles <gratno@gmail.com>
2022-11-27 14:09:16 -0500

    Ejercicio while

---
+++ java-primer-paso/src/WhileFernando.java
@@ -0,0 +1,13 @@
+public class WhileFernando {
+
+    public static void main(String[] args) {
+
+    	int contador = 1;
+    	
+        while (contador <= 10) {
+            //int contador = 1;            
+            System.out.println(contador);
+            contador++;
+        }
+    }
+}
