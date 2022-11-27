75171c95f4fad125350a353ca2280e97bb99170e
Milton Figueredo Miles <gratno@gmail.com>
2022-11-27 13:29:30 -0500

    Analizando While

--- java-primer-paso/src/EjemploCiclos.java
+++ java-primer-paso/src/EjemploCiclos.java
@@ -3,13 +3,16 @@ public class EjemploCiclos {
 	public static void main(String[] args) {
 
 		int contador = 0;
+		int contador1 = 10;
 
-		while (contador <= 10) {
+		//while(contador <= 10) {
+		while (contador <= 10 || contador1 <= 20) {
 			System.out.println(contador);
 			
 			//contador = contador + 1;
 			//contador += 1;
 			contador ++;
+			contador1 ++;
 			
 		}
 
