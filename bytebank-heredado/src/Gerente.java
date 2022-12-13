0d3aa8a1a8085ecfe291b0397f1255bb72069b27
Milton Figueredo Miles <gratno@gmail.com>
2022-12-12 19:19:10 -0500

    Método Extends

--- bytebank-heredado/src/Gerente.java
+++ bytebank-heredado/src/Gerente.java
@@ -1,48 +1,58 @@
+//                    Extiende de
+public class Gerente extends Funcionario {
 
-public class Gerente {
-	
-	private String nombre;
-	private String documento;
-	private double salario;
-	
-	
-	public Gerente() {
-		
+	private String clave;
+
+	public void setClave(String clave) {
+		this.clave = clave;
 	}
 
-
-	public String getNombre() {
-		return nombre;
+	public boolean iniciarSecion(String clave) {
+		return clave == "12AB";
 	}
 
-
-	public void setNombre(String nombre) {
-		this.nombre = nombre;
-	}
-
-
-	public String getDocumento() {
-		return documento;
-	}
-
-
-	public void setDocumento(String documento) {
-		this.documento = documento;
-	}
-
-
-	public double getSalario() {
-		return salario;
-	}
-
-
-	public void setSalario(double salario) {
-		this.salario = salario;
-	}
-	
 	public double getBonificacion() {
-		
-		return this.salario;
-		
+
+		return super.getSalario();
 	}
+
+	// lo de abAjo ya esta en FUNCIONARIO
+
+	// private String nombre;
+	// private String documento;
+	// private double salario;
+
+	// public Gerente() {
+
+	// }
+
+	// public String getNombre() {
+	// return nombre;
+	// }
+
+	// public void setNombre(String nombre) {
+	// this.nombre = nombre;
+	// }
+
+	// public String getDocumento() {
+	// return documento;
+	// }
+
+	// public void setDocumento(String documento) {
+	// this.documento = documento;
+	// }
+
+	// public double getSalario() {
+	// return salario;
+	// }
+
+	// public void setSalario(double salario) {
+	// this.salario = salario;
+	// }
+
+	// public double getBonificacion() {
+
+	// return this.salario;
+
+	// }
 }
--- bytebank-heredado/src/TestGerente.java
+++ bytebank-heredado/src/TestGerente.java
@@ -1,18 +1,19 @@
 
 public class TestGerente {
-	
-	
+
 	public static void main(String[] args) {
-		
-		//Gerente gerente = new Gerente();
-		//gerente.setSalario(5000);
-		Funcionario gerente = new Funcionario();
+
+		Gerente gerente = new Gerente();
+		// gerente.setSalario(5000);
+		// Funcionario gerente = new Funcionario();
 		gerente.setSalario(6000);
+		gerente.setNombre("Lorenza");
+		gerente.setClave("12AB");
 		gerente.setTipo(1);
-		
-		
+
 		System.out.println(gerente.getBonificacion());
 		System.out.println(gerente.getSalario());
+		System.out.println(gerente.iniciarSecion("12AB"));
 	}
 
 }
