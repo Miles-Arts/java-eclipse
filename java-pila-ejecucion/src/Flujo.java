3cb2969a03e7fd3b9c7857ef4f37d3a8789c11cb
Milton Figueredo Miles <gratno@gmail.com>
2022-12-23 15:37:32 -0500

    Controlando errores

--- java-pila-ejecucion/src/Flujo.java
+++ java-pila-ejecucion/src/Flujo.java
@@ -17,21 +17,24 @@ public class Flujo {
 		System.out.println("Inicio del metodo2");
 		for (int i = 1; i <= 5; i++) {
 			System.out.println(i);
-			
+
 			try { // Intenta con esto
-				//int num = 0;
-				//int resultado = i/num;
-				//System.out.println(resultado);
+
+				if (i == 3) {
+
+					int num = 0;
+					int resultado = i / num;
+					System.out.println(resultado);
+				}
+
 				String test = null;
 				System.out.println(test.toString());
-			} catch(ArithmeticException | NullPointerException exception) { // Atrapa el error
+			} catch (ArithmeticException | NullPointerException exception) { // Atrapa el error
 				System.out.println("Atrapo Excepción");
 				System.out.println(exception.getMessage());
 				exception.printStackTrace();
-			} 
-			
+			}
 		}
 		System.out.println("Fin del metodo2");
 	}
-
 }
