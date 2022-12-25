dd459b3cd54fe2189f6126ebfb448c5ae08f1319
Milton Figueredo Miles <gratno@gmail.com>
2022-12-25 17:23:49 -0500

    Creando Archivo JAR para Java

--- bytebank-heredado/src/com/bytebank/modelo/Cuenta.java
+++ bytebank-heredado/src/com/bytebank/modelo/Cuenta.java
@@ -1,123 +1,116 @@
 package com.bytebank.modelo;
 
-
-
 /**
- * Mi primera documentación en JAVA
+ * Mi primera documentación en JAVA Y se llama JAVAdoc
  * 
  * 
  * Cuenta va a crear nuevas instacias de CuentaCorriente
  * 
  * 
  * @version 1.0
- * @Uso  Educativo
+ * @Uso Educativo
  * @author Milton Figueredo
  *
  */
 
+public abstract class Cuenta {
 
-public abstract class  Cuenta {
-
-	
 	// MODIFICADORES DE AccESO
-	// public  = Accesible desde cualquier parte //El mas permisivo // 
+	// public = Accesible desde cualquier parte //El mas permisivo //
 	// default = Accesible dentro del paquete // Sólo visible a nivel Package
 	// protected = Default + clases hijas // Visible en Package y Herencia
 	// private = Sólo desde la clase misma // El más restrictivo
 
-	//private double saldo;
+	// private double saldo;
 	protected double saldo;
 	private int agencia;
 	private int numero;
 	private Cliente titular = new Cliente();
-	////private static int total = 0;
+	//// private static int total = 0;
 	private static int total;
-	
-	
+
 	/**
 	 * 
 	 * Instancia una nueva sin parámetros
 	 * 
 	 */
-	
+
 	public Cuenta() {
-		
+
 	}
-	
-	
+
 	/**
 	 * Instacia una cuenta usando agencia y número
 	 * 
 	 * @param agencia
 	 * @param numero
 	 */
-	
-	public Cuenta(int agencia, int numero) {
-		
-	        this.agencia = agencia;
-	        this.numero = numero;
-	        System.out.println("Estoy creando una cuenta " + this.numero);
 
-	        Cuenta.total ++;
-	    }
-		//if (agencia <= 0 ) {
-			//System.out.println("No se permite cuentas con 0 o menores a 0");
-			//this.agencia = 1;
-		//} else {
-		//	this.agencia = agencia;
-		//}
-		
-		//total++;
-		
-		//System.out.println("Se van creando: " + total + " cuentas");
-		
-		//System.out.println("Aqui se crea una nueva cuenta");
-	//}
+	public Cuenta(int agencia, int numero) {
+
+		this.agencia = agencia;
+		this.numero = numero;
+		System.out.println("Estoy creando una cuenta " + this.numero);
+
+		Cuenta.total++;
+	}
+	// if (agencia <= 0 ) {
+	// System.out.println("No se permite cuentas con 0 o menores a 0");
+	// this.agencia = 1;
+	// } else {
+	// this.agencia = agencia;
+	// }
+
+	// total++;
+
+	// System.out.println("Se van creando: " + total + " cuentas");
+
+	// System.out.println("Aqui se crea una nueva cuenta");
+	// }
 
 	// No retorna VALOR
-	public abstract void depositar(double valor); //{
+	public abstract void depositar(double valor); // {
 
-		// Esta cuenta Esta Cuenta
-		// this account this account
-		// this object
-		// this.saldo = this.saldo + valor;
-		//this.saldo += valor;
+	// Esta cuenta Esta Cuenta
+	// this account this account
+	// this object
+	// this.saldo = this.saldo + valor;
+	// this.saldo += valor;
 
-	//}
+	// }
 
-	
 	/**
-	 * Este método retira dinero de la cuenta y
-	 * si ocurre un error, devuelve una excepción.
+	 * Este método retira dinero de la cuenta y si ocurre un error, devuelve una
+	 * excepción.
+	 * 
 	 * @param valor
 	 * @throws SaldoInsuficienteException
 	 */
-	
+
 	// Retorna Valor
 	public void retirar(double valor) throws SaldoInsuficienteException {
-	//public boolean retirar(double valor) {
-		
+		// public boolean retirar(double valor) {
 
-		//if (this.saldo >= valor) {
-			// this.saldo = this.saldo - valor;
+		// if (this.saldo >= valor) {
+		// this.saldo = this.saldo - valor;
 		if (this.saldo < valor) {
-			//El control de ERRORES va al inicio del Método
+			// El control de ERRORES va al inicio del Método
 			throw new SaldoInsuficienteException("No tienes Saldo");
-			//this.saldo -= valor;
+			// this.saldo -= valor;
 		}
-			this.saldo -= valor;
+		this.saldo -= valor;
 
-		//	return true;
-		//}
-		//return false;
+		// return true;
+		// }
+		// return false;
 
 	}
-		
+
 	public boolean transferir(double valor, Cuenta cuenta) throws SaldoInsuficienteException {
 
 		if (this.saldo >= valor) {
 			this.retirar(valor);
-			//this.saldo = this.saldo - valor;
+			// this.saldo = this.saldo - valor;
 			// this.saldo -= valor;
 			cuenta.depositar(valor);
 			// cuenta.saldo = cuenta.saldo + valor;
@@ -139,24 +132,24 @@ public abstract class  Cuenta {
 		if (agencia > 0) {
 			this.agencia = agencia;
 		} else {
-			System.out.println("No esta permitido valores negativos") ;
+			System.out.println("No esta permitido valores negativos");
 		}
 	}
 
 	public int getAgencia() {
 		return agencia;
 	}
-	
+
 	public void setTitular(Cliente titular) {
 		this.titular = titular;
 	}
-	
+
 	public Cliente getTitular() {
 		return titular;
 	}
-	
+
 	public static int getTotal() {
-		
+
 		return Cuenta.total;
 	}
 }
