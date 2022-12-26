6a4e5f960644df1fe5e6202224d3635f67519d63
Milton Figueredo Miles <gratno@gmail.com>
2022-12-26 18:25:27 -0500

    Usando replace y concat para STRING

--- bytebank-heredado/src/com/bytebank/test/TestString.java
+++ bytebank-heredado/src/com/bytebank/test/TestString.java
@@ -5,12 +5,13 @@ public class TestString {
 	public static void main(String[] args) {
 		
 		//En JAVa STRING es un objeto y Java nos ayuda con una sixtais fácil de usar
-		String nombre = "Mercedes Benz"; 
+		String nombre = "mercedes benz"; 
 		//String nombre2 = new String("Clase E430"); //No se usa en el mundo REAL
 		
 		System.out.println(nombre);
-		nombre = nombre.replace("M" , "m");
-		nombre = nombre.replace("B" , "b");
+		nombre = nombre.replace("m" , "M");
+		nombre = nombre.replace("b" , "B");
+		nombre = nombre.concat(" - Clase E430");
 		
 		//System.out.println(nombre + " " + nombre2);
 		System.out.println(nombre);
