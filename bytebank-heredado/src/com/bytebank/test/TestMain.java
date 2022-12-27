bf7ddfa762a2cbf275026a059f935667ea1f63d0
Milton Figueredo Miles <gratno@gmail.com>
2022-12-27 12:49:50 -0500

    Arrays indice con JAVA

---
+++ bytebank-heredado/src/com/bytebank/test/TestMain.java
@@ -0,0 +1,24 @@
+package com.bytebank.test;
+
+public class TestMain {
+
+	public static void main(String[] args) {
+		
+		int edad = 20;
+		int edad2 = 15;
+		int edad3 = 67;
+		
+		//como hago paa agruparlas en 1 referencia?
+		
+		//Indices, número de posición  de cada sección del array
+		//numero del indice:  0   1   2   3   4
+		//		array       [ 0 | 1 | 2 | 3 | 4 ]
+		int[] edades = new int[5];
+		edades[2] = 30; 
+		
+		System.out.println(edades[2]);
+		System.out.println(edades[3]);
+	}
+	
+}
+
