60a30c90b729f697bff80a96ef143762554448f7
Milton Figueredo Miles <gratno@gmail.com>
2022-12-28 12:58:40 -0500

    Creando ARRAY para guardar cuentas

---
+++ bytebank-heredado/src/com/bytebank/modelo/GuardaCuentas.java
@@ -0,0 +1,17 @@
+package com.bytebank.modelo;
+
+public class GuardaCuentas {
+	
+	/**
+	 * Creando OBJETO para guardas muchas cuentas
+	 * 
+	 * Y Permitirnos agragar cuentas con un método
+	 * 
+	 * guardaCuentas.adiciona(cuenta)
+	 */
+	
+	public static void main(String[] args) {
+		
+	}
+
+}
