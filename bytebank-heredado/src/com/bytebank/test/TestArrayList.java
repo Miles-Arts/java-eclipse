a549bbc26d06bb04962c89b6503a29aee9186e6a
Milton Figueredo Miles <gratno@gmail.com>
2022-12-28 20:13:26 -0500

    Usando operador DIAMANTE <>

--- bytebank-heredado/src/com/bytebank/test/TestArrayList.java
+++ bytebank-heredado/src/com/bytebank/test/TestArrayList.java
@@ -1,6 +1,7 @@
 package com.bytebank.test;
 
 import java.util.ArrayList;
+import java.util.Iterator;
 
 import com.bytebank.modelo.Cuenta;
 import com.bytebank.modelo.CuentaCorriente;
@@ -8,26 +9,36 @@ import com.bytebank.modelo.CuentaCorriente;
 public class TestArrayList {
 
 	public static void main(String[] args) {
-		
-		
-		//Array List se puede usar sin <>
-		//ArrayList lista = new ArrayList<>();
-		
-		ArrayList lista = new ArrayList();
-		
-		Cuenta cuenta = new CuentaCorriente(11, 22);
-		Cuenta cuenta2 = new CuentaCorriente(13, 42);
-		
-		lista.add(cuenta);
-		lista.add(cuenta2);
-		
-		//     se pone el cats para pasar de Lista a CUENTA  
-		Cuenta ObtenerCuenta = (Cuenta) lista.get(0);
-		
-		System.out.println(ObtenerCuenta);
-		//System.out.println(lista.add(cuenta2));
-		
-		
-	}
 
+		// Array List se puede usar sin <> operador DIAMANTE
+		// ArrayList lista = new ArrayList<>();
+
+		// Forzar <> a que acepte sólo un tipo de objeto
+		// -Generics- Espacios que tiene un objeto como parámetros
+		// de la construcción de un objeto, Generics acepta un parámatro por otro
+		ArrayList<Cuenta> lista = new ArrayList<>();
+
+		Cuenta cuentaCorriente1 = new CuentaCorriente(88, 99);
+		Cuenta cuentaCorriente2 = new CuentaCorriente(13, 42);
+
+		lista.add(cuentaCorriente1);
+		lista.add(cuentaCorriente2);
+
+		// se pone el cats para pasar de Lista a CUENTA
+		Cuenta obtenerCuenta = (Cuenta) lista.get(0);
+
+		// System.out.println(ObtenerCuenta);
+		// System.out.println(lista.add(cuenta2));
+
+		// Un método para llamar lista Array con ARRAYList
+		// for ( int i = 0; i < lista.size(); i++) {
+		// System.out.println((Cuenta) lista.get(i));
+		// }
+
+		// por cada cuenta en la lista imprime
+		// FOR se usa operador <> DIAMANTE
+		for (Cuenta cuenta : lista) {
+			System.out.println(cuenta);
+		}
+	}
 }
