b08745c43c4ef9e4284e57175a2d05a75b1b30d2
Milton Figueredo Miles <gratno@gmail.com>
2023-01-16 19:53:23 -0500

    Solucionando erros file

--- bytebank-heredado/src/com/bytebank/test/TestRepasoArray.java
+++ bytebank-heredado/src/com/bytebank/test/TestRepasoArray.java
@@ -1,5 +1,16 @@
 package com.bytebank.test;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class TestRepasoArray {
+
+	public static void main(String[] args) {
+
+		int[] numeros = new int[10];
+
+		List lista = new ArrayList();
+		System.out.println("Hola");
+	}
 
 }
