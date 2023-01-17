3c48af37da1f17ba1a4c121a18a4c99c294a98f1
Milton Figueredo Miles <gratno@gmail.com>
2023-01-16 20:09:34 -0500

    Integer dejará de ser utilizado

--- bytebank-heredado/src/com/bytebank/test/TestRepasoArray.java
+++ bytebank-heredado/src/com/bytebank/test/TestRepasoArray.java
@@ -9,8 +9,20 @@ public class TestRepasoArray {
 
 		int[] numeros = new int[10];
 
-		List lista = new ArrayList();
-		System.out.println("Hola");
+		//List lista = new ArrayList();
+		
+		int numero = 40;
+		Integer numeroObjeto = new Integer(40);
+		
+		List<Integer> lista = new ArrayList<Integer>();
+		lista.add(numero); //Autoboxing
+		lista.add(new Integer(40));
+		lista.add(numeroObjeto);
+		
+
+		//PRIMITIVO != OBJECT	
+		
+		//System.out.println(lista.add.get());
 	}
 
 }
