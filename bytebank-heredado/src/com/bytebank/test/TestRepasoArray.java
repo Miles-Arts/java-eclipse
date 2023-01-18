0c4826acaf096836dbdc0980cd2edc54a0ac7920
Milton Figueredo Miles <gratno@gmail.com>
2023-01-17 19:51:37 -0500

    wrappers

--- bytebank-heredado/src/com/bytebank/test/TestRepasoArray.java
+++ bytebank-heredado/src/com/bytebank/test/TestRepasoArray.java
@@ -1,6 +1,7 @@
 package com.bytebank.test;
 
 import java.util.ArrayList;
+
 import java.util.List;
 
 public class TestRepasoArray {
@@ -14,7 +15,7 @@ public class TestRepasoArray {
 		int numero = 40;
 		//WARPPER
 		//Integer numeroObjeto = new Integer(40); DEPRECADO  Integer será desactualizado por JAVA
-		Integer numeroObjeto = new Integer.valueOf(40);
+		Integer numeroObjeto = Integer.valueOf(40);
 		
 		List<Integer> lista = new ArrayList<Integer>();
 		lista.add(numero); //Autoboxing
@@ -27,6 +28,18 @@ public class TestRepasoArray {
 		
 		
 		int valorPrimitivo = numeroObjeto.intValue();
+		
+		byte byteInteger = numeroObjeto.byteValue();
+		double doubleInteger = numeroObjeto.doubleValue();
+		float floatInteger = numeroObjeto.floatValue();
+		
+		
+		//El numero ENTERO más alto y bajo  que puede guardar
+		System.out.println(Integer.MAX_VALUE);
+		System.out.println(Integer.MIN_VALUE);
+		
+		System.out.println(Integer.SIZE); //BITS
+		System.out.println(Integer.BYTES); //BYTES
 		
 		//PRIMITIVO != OBJECT	
 		
