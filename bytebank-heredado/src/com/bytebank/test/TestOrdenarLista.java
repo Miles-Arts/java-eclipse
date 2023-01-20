b07e3d18e49aadec996bf25b7935679d929492c7
Milton Figueredo Miles <gratno@gmail.com>
2023-01-20 13:04:39 -0500

    LAMBDAS

--- bytebank-heredado/src/com/bytebank/test/TestOrdenarLista.java
+++ bytebank-heredado/src/com/bytebank/test/TestOrdenarLista.java
@@ -76,8 +76,6 @@ public class TestOrdenarLista {
 		// lista.sort(comparator);
 		// Comparator<T>*/
 
-	
-
 		// System.out.println("Después de ordenar");
 		// for (Cuenta cuenta : lista) {
 		// System.out.println(cuenta);
@@ -86,28 +84,28 @@ public class TestOrdenarLista {
 
 		// forma ANTIGUA
 		// lista.sort(new OrdenandoPorNombreTitular());
-		//Comparator<Cuenta> comparator = new OrdenadorPorNumeroCuenta();// Implementación de la intefaz
+		// Comparator<Cuenta> comparator = new OrdenadorPorNumeroCuenta();//
+		// Implementación de la intefaz
 		// lista.sort(comparator);
-		//lista.sort(new Comparator<Cuenta>() {
+		// lista.sort(new Comparator<Cuenta>() {
 
-			//@Override
-			//public int compare(Cuenta o1, Cuenta o2) {
-			//	return Integer.compare(o1.getNumero(), o2.getNumero());
-				// return 0;
-			//}
+		// @Override
+		// public int compare(Cuenta o1, Cuenta o2) {
+		// return Integer.compare(o1.getNumero(), o2.getNumero());
+		// return 0;
+		// }
 
-		//});
+		// });
 		// FORMA ANTIGUA
 		Collections.sort(lista, new Comparator<Cuenta>() {
 
 			@Override
 			public int compare(Cuenta o1, Cuenta o2) {
-				
+
 				return o1.getTitular().getNombre().compareTo(o2.getTitular().getNombre());
 			}
-			
+
 		});
-		
 
 		System.out.println("Después de ordenar por nombre titular");
 		for (Cuenta cuenta : lista) {
@@ -136,7 +134,11 @@ public class TestOrdenarLista {
 	}
 }
 
+
+
+
 // creando class para ejmplo de clase
+
 class OrdenadorPorNumeroCuenta implements Comparator<Cuenta> {
 
 	@Override
