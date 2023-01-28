a5da2592c268f8b1e0e2168ba1e8415d67bc057f
Milton Figueredo Miles <gratno@gmail.com>
2023-01-27 19:18:14 -0500

    Views Java

--- control-de-stock/src/main/java/com/alura/tests/PruebaConexion.java
+++ control-de-stock/src/main/java/com/alura/tests/PruebaConexion.java
@@ -8,11 +8,14 @@ public class PruebaConexion {
 
 	public static void main(String[] args) throws SQLException {
 		
+		System.out.println("Abriendo Conexión");
+		
 		Connection con = DriverManager.getConnection(
 				"jdbc:mysql://localhost/control_de_stock?useTimeZone=true&serverTimeZone=UTC", 
 				"root", 
 				"github");
 		
+		System.out.println("Actualizando...");
 		System.out.println("Cerrando conexión");
 		
 		con.close();
