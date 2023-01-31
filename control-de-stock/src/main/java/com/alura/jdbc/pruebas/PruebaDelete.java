59327725de8e3cfa202eaf4fd93fb8b05583caa0
Milton Figueredo Miles <gratno@gmail.com>
2023-01-30 19:27:30 -0500

    Creando PruebaDelete

--- control-de-stock/src/main/java/com/alura/jdbc/pruebas/PruebaDelete.java
+++ control-de-stock/src/main/java/com/alura/jdbc/pruebas/PruebaDelete.java
@@ -1,5 +1,24 @@
 package com.alura.jdbc.pruebas;
 
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+import com.alura.jdbc.factory.ConnectionFactory;
+
 public class PruebaDelete {
+	
+	public static void main(String[] args) throws SQLException {
+		
+		Connection con = new ConnectionFactory().recuperaConexion();
+
+		Statement statement = con.createStatement();
+		
+		statement.execute("DELETE FROM PRODUCTO WHERE ID = 99");
+		
+	
+		System.out.println(statement.getUpdateCount());
+		
+	}
 
 }
