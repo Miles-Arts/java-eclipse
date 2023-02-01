d05c1b6d4b4ff069d2ad9fe281a87b8acc98b557
Milton Figueredo Miles <gratno@gmail.com>
2023-02-01 13:50:04 -0500

    Creando un pool de conexiones Connection Factory

--- control-de-stock/src/main/java/com/alura/jdbc/factory/ConnectionFactory.java
+++ control-de-stock/src/main/java/com/alura/jdbc/factory/ConnectionFactory.java
@@ -1,11 +1,32 @@
 package com.alura.jdbc.factory;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
+import java.sql.Connection; 
 import java.sql.SQLException;
+
+import javax.sql.DataSource;
+
+import com.mchange.v2.c3p0.ComboPooledDataSource;
 
 public class ConnectionFactory {
 
+	
+	private DataSource datasource;
+	
+	public ConnectionFactory() {
+
+		
+		//creando POOL Conections
+		var pooledDataSource = new ComboPooledDataSource();
+		
+		pooledDataSource.setJdbcUrl("jdbc:mysql://localhost/control_de_stock?useTimeZonetrue&serverTomeZone=UTC");
+		
+		pooledDataSource.setUser("root");
+		pooledDataSource.setPassword("github");
+		
+		this.datasource = pooledDataSource;
+		
+	}
+	
 	public Connection recuperaConexion() throws SQLException {
 		
 		//Devolver la conexion con RETURN
@@ -14,10 +35,7 @@ public class ConnectionFactory {
 				//"root", 
 				//"github");
 		
-		return DriverManager.getConnection(
-				"jdbc:mysql://localhost/control_de_stock?useTimeZonetrue&serverTomeZone=UTC", 
-				"root", 
-				"github");
+		return this.datasource.getConnection();
 		
 	}
 	
