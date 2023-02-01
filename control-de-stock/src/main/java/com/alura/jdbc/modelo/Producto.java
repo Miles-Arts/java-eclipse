32ef13ff16bc3781ca43d7b8af09cbc7600f2d67
Milton Figueredo Miles <gratno@gmail.com>
2023-02-01 17:46:36 -0500

    Creando el modelo producto

---
+++ control-de-stock/src/main/java/com/alura/jdbc/modelo/Producto.java
@@ -0,0 +1,22 @@
+package com.alura.jdbc.modelo;
+
+public class Producto {
+	
+	
+	public static void main(String[] args) {
+		
+		
+		private Integer id;
+		
+		
+		private String nombre;
+		
+		
+		private String descripcion;
+		
+		
+		private Integer cantidad;
+		
+	}
+
+}
