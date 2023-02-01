2c3bcaea0e5b8497b2a2a27d42c3692cda369d12
Milton Figueredo Miles <gratno@gmail.com>
2023-02-01 17:17:50 -0500

    Probando el pool de conexiones

--- control-de-stock/src/main/java/com/alura/jdbc/pruebas/PruebaPoolDeConexiones.java
+++ control-de-stock/src/main/java/com/alura/jdbc/pruebas/PruebaPoolDeConexiones.java
@@ -1,21 +1,22 @@
 package com.alura.jdbc.pruebas;
 
 import java.sql.Connection;
+import java.sql.SQLException;
 
 import com.alura.jdbc.factory.ConnectionFactory;
 
 public class PruebaPoolDeConexiones {
 	
-	public static void main(String[] args) {
+	public static void main(String[] args) throws SQLException {
 		
 		
 		ConnectionFactory connectionFactory = new ConnectionFactory();
 		
-		for (int i = 0; i < 0; i++) {
+		for (int i = 0; i < 20; i++) {
 			
 			Connection conexion = connectionFactory.recuperaConexion();
 			
-			System.out.print("Abriendo la conexión de número " + i +1);
+			System.out.println("Abriendo la conexión de número " + (i + 1));
 			
 		}
 		
