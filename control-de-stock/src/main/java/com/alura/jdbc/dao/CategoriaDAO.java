a12540f5e0e60902251b456d197f17b58dfaaef3
Milton Figueredo Miles <gratno@gmail.com>
2023-02-02 18:48:49 -0500

    Creando lógica para categorias con DAO

--- control-de-stock/src/main/java/com/alura/jdbc/dao/CategoriaDAO.java
+++ control-de-stock/src/main/java/com/alura/jdbc/dao/CategoriaDAO.java
@@ -1,6 +1,9 @@
 package com.alura.jdbc.dao;
 
 import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -20,6 +23,26 @@ public class CategoriaDAO {
 	public List<Categoria> listar() {
 
 		List<Categoria> resultado = new ArrayList<>();
+		
+		try {
+			final PreparedStatement statement = con.prepareStatement(
+					"SELECT ID, NOMBRE FROM CATEGORIA");
+			
+			try(statement) {
+				
+			final 	ResultSet resultSet = statement.executeQuery();
+				
+			try(resultSet) {
+				
+			};
+			
+		}
+		
+	} catch (SQLException e) {
+			
+			throw new RuntimeException(e);
+			
+		}
 		
 		return resultado;
 	}
